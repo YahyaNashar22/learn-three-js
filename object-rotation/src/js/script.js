c2f3d9bdd9a702f9a270b3572b0c8cd427baf9e3
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-22 00:31:31 +0300

    added boilerplate code to set up three with orbit controls, animation loop, resize listener, and stars background

--- object-rotation/src/js/script.js
+++ object-rotation/src/js/script.js
@@ -14,3 +14,54 @@ import uranusTexture from '../img/uranus.jpg';
 import uranusRingTexture from '../img/uranus ring.png';
 import neptuneTexture from '../img/neptune.jpg';
 import plutoTexture from '../img/pluto.jpg';
+
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(-90, 140, 140);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+]);
+
+
+const textureLoader = new THREE.TextureLoader();
+
+
+function animate() {
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+    camera.aspect = this.window.innerWidth / this.window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+});
+
+
