71d440d553ca5f44cc87d2d49489fb74c01b2e6c
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-22 01:29:36 +0300

    added saturn and its ring and fixed pointlight to illuminate properly

--- object-rotation/src/js/script.js
+++ object-rotation/src/js/script.js
@@ -51,8 +51,62 @@ scene.background = cubeTextureLoader.load([
 
 const textureLoader = new THREE.TextureLoader();
 
+const sunGeo = new THREE.SphereGeometry(16, 30, 30);
+const sunMat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(sunTexture),
+});
+const sun = new THREE.Mesh(sunGeo, sunMat);
+scene.add(sun);
+
+
+
+const mercuryGeo = new THREE.SphereGeometry(3.2, 30, 30);
+const mercuryMat = new THREE.MeshStandardMaterial({
+    map: textureLoader.load(mercuryTexture)
+});
+const mercury = new THREE.Mesh(mercuryGeo, mercuryMat);
+const mercuryObj = new THREE.Object3D();
+// * We appended it to the mercuryObj, so it's position is now relative to it
+mercuryObj.add(mercury);
+scene.add(mercuryObj);
+mercury.position.x = 28;
+
+
+
+const saturnGeo = new THREE.SphereGeometry(10, 30, 30);
+const saturnMat = new THREE.MeshStandardMaterial({
+    map: textureLoader.load(saturnTexture)
+});
+const saturn = new THREE.Mesh(saturnGeo, saturnMat);
+const saturnObj = new THREE.Object3D();
+saturnObj.add(saturn);
+scene.add(saturnObj);
+saturn.position.x = 138;
+
+const saturnRingGeo = new THREE.RingGeometry(10, 20, 32);
+const saturnRingMat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(saturnRingTexture),
+    side: THREE.DoubleSide
+});
+const saturnRing = new THREE.Mesh(saturnRingGeo, saturnRingMat);
+saturn.add(saturnRing);
+saturn.position.x = 138;
+saturnRing.rotation.x = -0.5 * Math.PI;
+
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 10000, 100000);
+scene.add(pointLight);
 
 function animate() {
+    // make planets rotate
+    sun.rotateY(0.004);
+    mercury.rotateY(0.004);
+    mercuryObj.rotateY(0.04);
+    saturn.rotateY(0.038);
+    saturnObj.rotateY(0.0009);
+
+
+
     renderer.render(scene, camera);
 }
 
