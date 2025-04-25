24e587086cd4883177bc3107036f4344321b2876
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-25 07:32:41 +0300

    GLSL is HARD!

--- shader-handon/src/js/script.js
+++ shader-handon/src/js/script.js
@@ -19,20 +19,43 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(-90, 140, 140);
+camera.position.set(0, 0, 12);
 orbit.update();
+
+// object containing different uniform values to be passed to the shaders
+// contains type --ex: float, and the value
+const uniforms = {
+    u_time: { type: 'f', value: 0.0 },
+    u_resolution: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight).multiplyScalar(window.devicePixelRatio) },
+    u_mouse: {
+        type: 'v2',
+        value: new THREE.Vector2(0.0, 0.0)
+    },
+    image: {
+        type: 't',
+        value: new THREE.TextureLoader().load(nebula)
+    }
+}
+
+window.addEventListener('mousemove', function (e) {
+    uniforms.u_mouse.value.set(e.screenX / this.window.innerWidth, 1 - e.screenY / this.window.innerHeight);
+})
 
 const geometry = new THREE.PlaneGeometry(10, 10, 30, 30);
 const material = new THREE.ShaderMaterial({
     vertexShader: document.getElementById('vertexShader').textContent,
     fragmentShader: document.getElementById('fragmentShader').textContent,
-    wireframe: true
+    wireframe: false,
+    side: THREE.DoubleSide,
+    uniforms
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-
+const clock = new THREE.Clock();
 function animate() {
+    // get the value from the uniforms field u_time, and update it
+    uniforms.u_time.value = clock.getElapsedTime();
     renderer.render(scene, camera);
 }
 
