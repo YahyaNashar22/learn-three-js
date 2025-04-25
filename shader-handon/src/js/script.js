6945e2ca0918ecc3be8a12f3aa037ee2cc9ed610
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-25 04:57:15 +0300

    added simple code ot generate shaders

--- shader-handon/src/js/script.js
+++ shader-handon/src/js/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
+const nebula = new URL('../assets/nebula.jpg', import.meta.url).href;
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -19,6 +21,15 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 
 camera.position.set(-90, 140, 140);
 orbit.update();
+
+const geometry = new THREE.PlaneGeometry(10, 10, 30, 30);
+const material = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    wireframe: true
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 
 function animate() {
