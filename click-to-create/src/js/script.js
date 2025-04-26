98fa01c2b5f3eb30708c09ffd8682271ca87de18
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-26 18:40:05 +0300

    created necessarly components

--- click-to-create/src/js/script.js
+++ click-to-create/src/js/script.js
@@ -17,9 +17,28 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(-90, 140, 140);
+camera.position.set(0, 6, 6);
 orbit.update();
 
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+scene.add(directionalLight);
+
+const helper = new THREE.AxesHelper(20);
+scene.add(helper);
+
+// represents the normalized position of the cursor
+const mouse = new THREE.Vector2();
+// hold the coordinates where the plane intersects with the plane
+const intersectionPoint = new THREE.Vector3();
+// indicates the direction of the plane
+const planeNormal = new THREE.Vector3();
+// plane to be created on each cursor move change
+const plane = new THREE.Plane();
+// emit the ray between the camera and the cursor
+const raycaster = new THREE.Raycaster();
 
 function animate() {
     renderer.render(scene, camera);
