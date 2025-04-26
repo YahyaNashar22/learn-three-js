48de3cdf7d58c17a571934aa656c4b75ea2873ac
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-26 12:19:52 +0300

    created the physics world, time, and body for the plane

--- canonJS/src/js/script.js
+++ canonJS/src/js/script.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
+import * as CANNON from 'cannon-es';
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -17,11 +19,36 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(-90, 140, 140);
+camera.position.set(0, 20, -30);
 orbit.update();
+
+const groundGeo = new THREE.PlaneGeometry(30, 30);
+const groundMat = new THREE.MeshBasicMaterial({
+    colo: 0xFFFFFF,
+    side: THREE.DoubleSide,
+    wireframe: true
+});
+const groundMesh = new THREE.Mesh(groundGeo, groundMat);
+scene.add(groundMesh);
+
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.81, 0)
+});
+
+const groundBody = new CANNON.Body({
+    shape: new CANNON.Plane()
+});
+world.add(groundBody);
+
+const timeStep = 1 / 60;
 
 
 function animate() {
+    world.step(timeStep);
+    
+    groundMesh.position.copy(groundBody.position);
+    groundMesh.quaternion.copy(groundBody.quaternion);
+
     renderer.render(scene, camera);
 }
 
